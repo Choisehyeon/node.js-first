d31979c9e2397daffc1193f533b2f616c334fcd8
Choisehyeon <chltpgus6275@naver.com>
2021-12-22 16:15:50 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://chltpgus:qltmxm1016@firsttest.sls93.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at ${port}!`))
